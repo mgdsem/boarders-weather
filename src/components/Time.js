742d0ff0999b2eb6ac79b6be2f957dbab9d540ba
Magdalena Semik <mgd.kaczmarek@gmail.com>
2019-03-02 20:50:52 +0100

    make header

---
+++ src/components/Time.js
@@ -0,0 +1,41 @@
+import React, { Component } from 'react';
+import moment from 'moment';
+import { className } from 'postcss-selector-parser';
+
+class Time extends Component {
+    constructor(props) {
+        super(props);
+
+        this.getCurrentTime = this.getCurrentTime.bind(this);
+
+        this.intervalid = null;
+
+        this.state = {
+            currentTime: moment().format("HH:mm:ss"),
+        };
+    };
+
+    componentDidMount() {
+        this.getCurrentTime();
+        this.intervalid = setInterval(this.getCurrentTime, 1000)
+    }
+
+    componentWillUnmount() {
+        clearInterval(this.intervalid);
+    }
+
+    getCurrentTime() {
+        this.setState({ currentTime: moment().format("HH:mm:ss") });
+    }
+
+    render() {
+        return (
+            <div className="time">
+                <p>{moment().format("MMM Do YY")}</p>
+                <p>{this.state.currentTime}</p>
+            </div>
+        );
+    }
+};
+
+export default Time;
