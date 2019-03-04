ab077f24984aa508b38ea30fe1a01ef580405c76
Magdalena Semik <mgd.kaczmarek@gmail.com>
2019-03-04 22:37:35 +0100

    prepare proper API call

--- src/api/index.js
+++ src/api/index.js
@@ -6,4 +6,6 @@ const api = axios.create({
     baseURL: 'https://api.openweathermap.org/data/2.5'
 })
 
-export const getWeather = (city) => api.get(`/weather?q=${city},pl&appid=${API_KEY}`);
+// export const getCurrentWeather = (city) => api.get(`/weather?q=${city},pl&appid=${API_KEY}`);
+
+export const getWeather = (city) => api.get(`/forecast?q=${city},pl&appid=${API_KEY}`);
--- src/components/mainDisplay.js
+++ src/components/mainDisplay.js
@@ -1,18 +1,69 @@
 import React, { Component } from 'react';
+import moment from 'moment';
 
 import { getWeather } from '../api';
+import Input from './Input';
+import Button from './Button';
 
 class MainDisplay extends Component {
+    constructor(props) {
+        super(props)
 
-    componentDidMount() {
-        getWeather('jastarnia').then((response) => {
-            console.log(response);
-        })
+        this.onCityChange = this.onCityChange.bind(this);
+        this.onCityChoose = this.onCityChoose.bind(this);
+
+        this.state = {
+            city: '',
+            weather: [],
+            isError: false,
+            currentWeather: {}
+        }
+    }
+
+    // componentDidMount() {
+    //     getCurrentWeather('jastarnia').then((response) => {
+    //         console.log(response);
+    //     })
+    // }
+
+    onCityChange(e) {
+        this.setState({
+            city: e.target.value
+        });
+    }
+
+    onCityChoose() {
+        if (this.state.city) {
+            getWeather(this.state.city)
+                .then((response) => {
+                    this.setState({
+                        weather: response.data.list,
+                        isError: false,
+                        currentWeather: response.data.list[0]
+                    })
+                })
+                .catch((error) => {
+                    this.setState({
+                        isError: true
+                    })
+                })
+        }
     }
 
     render() {
+        this.state.weather.forEach((hour) => {
+            console.log(moment(hour.dt * 1000).format());
+        })
+
+        console.log(this.state);
         return (
-            <div>placeholder</div>
+            <div>
+                <Input type="text" id="city" value={this.state.city} placeholder="choose a city" onChange={this.onCityChange} />
+                <Button onClick={this.onCityChoose}>Go!</Button>
+                {this.state.isError && (
+                    <p>There is no information about choosen city. Pleace choose another city. </p>
+                )}
+            </div>
         )
     }
 }
