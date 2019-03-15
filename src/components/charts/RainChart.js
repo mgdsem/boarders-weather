3164a647b94a691e0d75d81f3fe71ea2a3ce1288
Magdalena Semik <mgd.kaczmarek@gmail.com>
2019-03-15 22:29:24 +0100

    make refactoring and basics of rain chart

--- src/components/charts/RainChart.js
+++ src/components/charts/RainChart.js
@@ -4,11 +4,19 @@ const RainChart = ({
     rains
 }) => {
     return (
-        <div>
-            <h2>Rain</h2>
+        <div className="chart__wrapper">
+            <h2 className="chart-title chart-title--has-mb">Rain</h2>
             <ul className="list">
                 {rains.map((rain, i) => (
-                    <li key={`${rain.rain}-${i}`}>{rain.rain || 0} mm/3h</li>
+                    <li
+                        className="list__item chart"
+                        key={`${rain.rain}-${i}`}
+                    >
+                        <div className="chart__speed">
+                            {Number(rain.rain || 0).toFixed(4) || 0} mm/3h
+                        </div>
+                        <div className="chart__hour">{rain.hour}</div>
+                    </li>
                 ))}
             </ul>
         </div>
